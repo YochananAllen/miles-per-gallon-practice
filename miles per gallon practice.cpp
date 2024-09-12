e91f4686b471b99094dcf6c47b7a0db108ff3a2d
b2all <b2all@CAPSULECORP>
2024-09-12 07:57:48 -0400

    Add project files.

---
+++ miles per gallon practice.cpp
@@ -0,0 +1,34 @@
+/*
+*Title:Miles per Gallon
+* programmer: Yochanan Allen
+* Date: 09/12/24
+* 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const double miles = 375;
+    const double gallons = 15;
+    
+    double mpg = miles / gallons;
+
+     cout << "the car gets " << mpg << "miles per gallon " << endl;
+
+
+    return 0;
+    
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
